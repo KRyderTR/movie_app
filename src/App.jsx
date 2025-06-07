9d7f0c6270b290bf3a886fd76bb85ef633eb0704
KRyderTR <charitrom@gmail.com>
2025-06-07 18:29:32 +0300

    commit-api-connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+        const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+        
+        const response = await fetch(endpoint, API_OPTIONS);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="/hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
 
-    </>
-  )
-}
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
 
-export default App
+          <section className="all-molvies">
+            <h2>All movies</h2>
+
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
